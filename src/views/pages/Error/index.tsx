ac8da11f5c609d034950c64f34fbe6f59a7b6419
lucasssantos94 <lucas.ssantos94@outlook.com>
2025-11-04 16:39:29 -0300

    feat: added error page

--- src/views/pages/Error/index.tsx
+++ src/views/pages/Error/index.tsx
@@ -1,7 +1,23 @@
+import { isRouteErrorResponse, Link, useRouteError } from 'react-router'
+
 const ErrorPage = () => {
+  const error = useRouteError()
   return (
-    <div>
-      <h1>ErrorPage</h1>
+    <div className='min-h-screen flex items-center justify-center bg-foreground'>
+      <div className='max-w-md w-full text-center'>
+        <h1 className='text-4xl font-bold mb-4 text-background'>Oops!</h1>
+        <p className='text-lg text-gray-600 mb-4'>
+          {isRouteErrorResponse(error)
+            ? 'Página não encontrada'
+            : 'Algo deu errado'}
+        </p>
+        <Link
+          to='/'
+          className='inline-block bg-red-500 text-white px-6 py-2 rounded-lg hover:bg-red-600 transition-colors'
+        >
+          Voltar para Home
+        </Link>
+      </div>
     </div>
   )
 }
