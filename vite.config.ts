ef1ad465bbabf325d40631d33be7c2ce30a3bbb0
lucasssantos94 <lucas.ssantos94@outlook.com>
2025-11-04 15:00:31 -0300

    chore: configured tailwindcss and shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+import path from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@app': path.resolve(__dirname, './src/app'),
+      '@views': path.resolve(__dirname, './src/views'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+    },
+  },
+  server: {
+    open: true,
+    host: true,
+  },
 })
